03102297cef2720a2eb807bf5e00daac17cf67e1
Chase Mitchusson <cmtchssn@gmail.com>
2022-06-14 13:50:58 -0400

    removed bulk wwise files to make room on server.

--- Assets/Scripts/AudioScripts/AudioManager.cs
+++ Assets/Scripts/AudioScripts/AudioManager.cs
@@ -4,10 +4,78 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public uint playingID;
+    public static AudioManager instance;
 
-    public void menuSFX()
+    private void Awake()
     {
-        AkSoundEngine.PostEvent("MenuSelectHighlight", this.gameObject);
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic();
+    }
+
+    public void PlayMusic()
+    {
+        AkSoundEngine.PostEvent("Play_MusicTheme", gameObject);
+        // load music event 1 time
+        // do not destroy until game is destroyed
+    }
+
+    public void MusicState()
+    {
+        // get scene name
+        AkSoundEngine.SetState("GameState", "InMenu");
+        // set state based on scene name
+    }
+
+    public void PickupSFX()
+    {
+        // detect collision between playerChar and x
+        // play pickup event
+    }
+
+    public void HazardSFX()
+    {
+        // detect collision between playerChar and y
+        // play hazard event
+    }
+
+    public void VortexSFX()
+    {
+        // detect vortex generation
+        // play vortex event
+    }
+
+    public void BoostSFX()
+    {
+        // detect collision with booster
+        // play boost event
+    }
+
+    public void LapClearedSFX()
+    {
+        // detect new lap
+        // play lap cleared event
+    }
+
+    public void StageClearedSFX()
+    {
+        // detect player winning
+        // play stage cleared event
+    }
+
+    public void StatueSFX()
+    {
+        // detect statue status
+        // play statue event
     }
 }
