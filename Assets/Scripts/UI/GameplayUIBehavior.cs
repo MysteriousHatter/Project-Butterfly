e5eba039220deda189b085e76335b6ff19b5663d
darlynn-best <darlynn.best@gmail.com>
2022-06-20 16:42:51 -0500

    Revert "Progress on volume options"
    This reverts commit 382815ba0d0b53b0e7d8baacdcc9ba4d6fef1997.

--- Assets/Scripts/UI/GameplayUIBehavior.cs
+++ Assets/Scripts/UI/GameplayUIBehavior.cs
@@ -98,9 +98,6 @@ public class GameplayUIBehavior : MonoBehaviour
             //Fix BUG: Start of the game is always playing
             Time.timeScale = 1;
         }
-        
-        AudioListener.volume = PlayerPrefs.GetFloat("volume");
-
     }
 
     // Update is called once per frame
